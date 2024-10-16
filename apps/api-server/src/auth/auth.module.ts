4ce65963eb0b79bbc9892eaf600cf216e360dd8f
BGM-Karl <bgm.karllin@gmail.com>
2024-10-16 23:47:29 +0800

    fix: 修改大小寫

--- apps/api-server/src/auth/auth.module.ts
+++ apps/api-server/src/auth/auth.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { AccountModule } from '../account/account.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategy/local.strategy';
-import { JwtStrategy } from './strategy/jwt.strategy';
-import { JwtRefreshTokenStrategy } from './strategy/Jwt-refresh-token.strategy';
-import { UtilsModule } from '../utils/utils.module';
-import { LoggerModule } from '../logger/logger.module';
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { AccountModule } from '../account/account.module'
+import { LoggerModule } from '../logger/logger.module'
+import { UtilsModule } from '../utils/utils.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { JwtStrategy } from './strategy/jwt.strategy'
+import { JwtRefreshTokenStrategy } from './strategy/jwt-refresh-token.strategy'
+import { LocalStrategy } from './strategy/local.strategy'
 
 @Module({
   imports: [
